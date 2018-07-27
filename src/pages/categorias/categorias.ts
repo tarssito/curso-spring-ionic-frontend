bbaac7789a5b8ae1885bcc0499ff7be2f8f06bf9
Tarssito <tarssito@gmail.com>
2018-07-26 23:16:36 -0300

    Listando produtos de uma dada categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -30,8 +30,8 @@ export class CategoriasPage {
     );
   }
 
-  showProdutos() {
-    this.navCtrl.push('ProdutosPage');
+  showProdutos(categoriaId: string) {
+    this.navCtrl.push('ProdutosPage', { categoriaId : categoriaId });
   }
 
 }
--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 
 @IonicPage()
@@ -12,22 +13,18 @@ export class ProdutosPage {
 
   items: ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        nome: "Mouse",
-        preco: 80.99
-      },
-      {
-        id: "2",
-        nome: "Teclado",
-        preco: 100.00
-      }
-    ]
+    let categoria = this.navParams.get('categoriaId');
+    this.produtoService.findByCategoria(categoria)
+      .subscribe(response => {
+        this.items = response['content'];
+      }, error => {});
   }
 
 }
