9bcad29f95de91dcd02d8c8b2da8b1b7b8e199bb
Tarssito <tarssito@gmail.com>
2018-07-30 23:05:10 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -65,5 +69,18 @@ export class ProfilePage {
       this.camereOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => { });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -3,11 +3,14 @@ import { HttpClient} from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
+import { ImageUtilService } from "../image-util.service";
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http: HttpClient){
+    constructor(
+        public http: HttpClient,
+        public imageUtilService: ImageUtilService){
     }
 
     findById(id: string){
@@ -27,6 +30,20 @@ export class ClienteService {
         return this.http.post(
             `${API_CONFIG.baseUrl}/clientes`,
             obj,
+            {
+               observe: 'response',
+               responseType: 'text' 
+            }
+        );
+    }
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`,
+            formData,
             {
                observe: 'response',
                responseType: 'text' 
